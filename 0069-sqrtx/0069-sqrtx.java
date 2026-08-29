a477e05cdd7750f8cf7533c2edc5be6d0c327640
Gopi Krishna <gmamidi577@gmail.com>
2026-08-30 00:32:04 +0530

    Time: 1 ms (98.6%), Space: 42.8 MB (23.56%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        int low=0,high=x,mid=-1;
+        while(low<=high){
+             mid=low+(high-low)/2;
+            if(mid*mid==x)
+               return mid;
+            else if((long)mid*mid<x)
+               low=mid+1;
+             else
+             high=mid-1;
+        }
+        return high;
+    }
+}
